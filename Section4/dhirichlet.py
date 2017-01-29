b0fd3738cf356372ad63589baac9dfa0b9d4b392
野本　英梨子 <nomotoeriko@nomotoeinashi-no-MacBook-Pro.local>
2017-01-29 19:35:21 +0900

    可読性の向上を図り表現を変更(内容に変更なし)

--- Section4/dhirichlet.py
+++ Section4/dhirichlet.py
@@ -1,6 +1,6 @@
 from functions import dhirichlet as dhir
 from mpl_toolkits.mplot3d import Axes3D
-from pylab import *
+import pylab
 import numpy as np
 
 def draw(alpha1, alpha2, alpha3, name):
@@ -15,18 +15,18 @@ def draw(alpha1, alpha2, alpha3, name):
             x.append(xi)
             y.append(yi)
             z.append(dhir([xi, yi, zi], [alpha1, alpha2, alpha3]))
-    clf()
+    pylab.clf()
     npx = np.arange(101)*0.01
     npy = np.arange(101)*0.01
-    npX, npY = meshgrid(npx, npy)
+    npX, npY = pylab.meshgrid(npx, npy)
     npz = np.zeros((101, 101))-1
     for i in range(len(x)):
         npz[round(x[i]*100), round(y[i]*100)] = z[i]
-    pcolor(npX, npY, npz, cmap='Blues', vmin=-1, vmax=20)
-    colorbar()
-    xlim(0, 1)
-    ylim(0, 1)
-    savefig(name)
+    pylab.pcolor(npX, npY, npz, cmap='Blues', vmin=-1, vmax=20)
+    pylab.colorbar()
+    pylab.xlim(0, 1)
+    pylab.ylim(0, 1)
+    pylab.savefig(name)
 
 if __name__ == '__main__':
     x = []
@@ -41,9 +41,9 @@ if __name__ == '__main__':
             y.append(yi)
             z.append(dhir([xi, yi, zi], [4, 4, 4]))
     # Dir(4,4,4)をθ1, θ2を説明変数(Θ3=1-Θ1-Θ2)として3次元に可視化
-    ax = Axes3D(plt.figure())
+    ax = Axes3D(pylab.figure())
     ax.plot_wireframe(x, y, z)
-    savefig('Dir(4,4,4)3D.png')
+    pylab.savefig('Dir(4,4,4)3D.png')
 
     # Dir(α1,α2,α3)をθ1, θ2を説明変数(Θ3=1-Θ1-Θ2)として2次元に濃淡で可視化
     draw(1, 1, 1, 'Dir(1,1,1)2D.png')
